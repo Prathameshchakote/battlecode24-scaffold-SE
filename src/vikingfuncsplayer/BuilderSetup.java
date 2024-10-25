867f90212fcfdf1ea905528012b90051f313ba39
Akira <smedley@pdx.edu>
2024-10-25 12:12:05 -0700

    created builder setup file

---
+++ src/vikingfuncsplayer/BuilderSetup.java
@@ -0,0 +1,18 @@
+package vikingfuncsplayer;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class BuilderSetup {
+    public static final int Explore_Rounds = 150;
+
+    public static void runSetup(RobotController rc) throws GameActionException {
+        if(rc.getRoundNum() < Explore_Rounds){
+
+        }
+        else{
+            
+        }
+    }
+    
+}
