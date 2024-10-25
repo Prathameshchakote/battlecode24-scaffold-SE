7ada795943830fb7d8d99d3dd289302b696b9b0c
Akira <smedley@pdx.edu>
2024-10-25 12:54:38 -0700

    Initlize movement towards middle for builder

--- src/vikingfuncsplayer/BuilderSetup.java
+++ src/vikingfuncsplayer/BuilderSetup.java
@@ -1,18 +1,35 @@
 package vikingfuncsplayer;
 
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 
 public class BuilderSetup {
-    public static final int Explore_Rounds = 150;
+
+    static final Direction[] southbound ={
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST
+    };
+
+    static final Direction[] northbound ={
+        Direction.NORTHEAST,
+        Direction.NORTH,
+        Direction.NORTHWEST
+    };
 
     public static void runSetup(RobotController rc) throws GameActionException {
-        if(rc.getRoundNum() < Explore_Rounds){
+        String team = rc.getTeam().toString();
+        if(team == "A"){
+            for(int i = 0; i < 3; i++){
+                if(rc.canMove(southbound[i])) rc.move(southbound[i]);
+            }
+        } else{
+            for(int i = 0; i < 3; i++){
+                if(rc.canMove(northbound[i])) rc.move(northbound[i]);
+            }
+        }
 
-        }
-        else{
-            
-        }
     }
     
 }
