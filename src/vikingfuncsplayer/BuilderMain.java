f5fd022910c4fe157e942e4b48c53fade0bad01f
Akira <smedley@pdx.edu>
2024-11-02 11:43:53 -0700

    Began Builder Main phase code

---
+++ src/vikingfuncsplayer/BuilderMain.java
@@ -0,0 +1,11 @@
+package vikingfuncsplayer;
+
+import battlecode.common.*;
+
+public class BuilderMain {
+    
+    public static void runSetup(RobotController rc) throws GameActionException {
+
+    }
+
+}
