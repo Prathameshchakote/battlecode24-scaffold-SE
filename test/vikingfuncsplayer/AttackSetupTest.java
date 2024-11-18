11ab7a9d075f04fad78f75d1669969779616a6b7
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:05:11 -0800

    setup

--- test/vikingfuncsplayer/AttackSetupTest.java
+++ test/vikingfuncsplayer/AttackSetupTest.java
@@ -13,3 +13,17 @@ import battlecode.common.GameActionException;
 import battlecode.common.GameConstants;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+
+
+public class AttackSetupTest {
+    @Mock
+    private RobotController rc;
+
+    private MapLocation currentLocation;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        currentLocation = new MapLocation(0, 0);
+        when(rc.getLocation()).thenReturn(currentLocation);
+    }
