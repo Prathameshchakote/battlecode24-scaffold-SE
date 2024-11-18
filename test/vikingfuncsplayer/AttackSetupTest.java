ac9ace11a4885eb6bcdc4140b9f12e4e1871e9fb
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:05:50 -0800

    testRunSetup_WithCrumbs_CannotFill

--- test/vikingfuncsplayer/AttackSetupTest.java
+++ test/vikingfuncsplayer/AttackSetupTest.java
@@ -44,3 +44,23 @@ public class AttackSetupTest {
         verify(rc).canMove(any(Direction.class));
         verify(rc).move(any(Direction.class));
     }
+
+    @Test
+    public void testRunSetup_WithCrumbs_CannotFill() throws GameActionException {
+        // Setup
+        MapLocation crumbLocation = new MapLocation(1, 1);
+        when(rc.getRoundNum()).thenReturn(GameConstants.SETUP_ROUNDS - 1);
+        when(rc.senseNearbyCrumbs(-1)).thenReturn(new MapLocation[]{crumbLocation});
+        when(rc.canFill(crumbLocation)).thenReturn(false);
+        when(rc.canMove(any(Direction.class))).thenReturn(true);
+
+        // Execute
+        AttackSetup.runSetup(rc);
+
+        // Verify
+        verify(rc).getRoundNum();
+        verify(rc).senseNearbyCrumbs(-1);
+        verify(rc).canFill(crumbLocation);
+        verify(rc).canMove(any(Direction.class));
+        verify(rc).move(any(Direction.class));
+    }
