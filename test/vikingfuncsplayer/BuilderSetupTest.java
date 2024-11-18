07c877893714c1a4f51248f065d1cb0f3f889264
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:13:11 -0800

    testRunSetup_EarlyGame_NoFlags

--- test/vikingfuncsplayer/BuilderSetupTest.java
+++ test/vikingfuncsplayer/BuilderSetupTest.java
@@ -21,3 +21,17 @@ public class BuilderSetupTest {
         when(rc.getLocation()).thenReturn(currentLocation);
         when(mockFlag.getLocation()).thenReturn(new MapLocation(1, 1));
     }
+
+    @Test
+    public void testRunSetup_EarlyGame_NoFlags() throws GameActionException {
+        // Setup - Early game (round < 100)
+        when(rc.getRoundNum()).thenReturn(50);
+        when(rc.senseNearbyFlags(-1)).thenReturn(new FlagInfo[]{});
+
+        // Execute
+        BuilderSetup.runSetup(rc);
+
+        // Verify exploration happens when no flags are nearby
+        verify(rc).senseNearbyFlags(-1);
+        verifyNoInteractions(mockMapInfo);
+    }
