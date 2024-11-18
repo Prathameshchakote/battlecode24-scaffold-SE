49bf8947a2f527c0f8be54e645ddb46463e7c4d0
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:09:13 -0800

    testRunMain_WithUnpickedFlag

--- test/vikingfuncsplayer/BuilderMainTest.java
+++ test/vikingfuncsplayer/BuilderMainTest.java
@@ -18,3 +18,22 @@ public class BuilderMainTest {
         directions = Direction.values();
         when(rc.getLocation()).thenReturn(currentLocation);
     }
+
+    @Test
+    public void testRunMain_WithUnpickedFlag() throws GameActionException {
+        // Set up flag info
+        MapLocation flagLocation = new MapLocation(12, 12);
+        FlagInfo flag = mock(FlagInfo.class);
+        when(flag.getLocation()).thenReturn(flagLocation);
+        when(flag.isPickedUp()).thenReturn(false);
+        when(rc.senseNearbyFlags(-1)).thenReturn(new FlagInfo[]{flag});
+
+        // Set up building conditions
+        when(rc.canBuild(any(TrapType.class), any(MapLocation.class))).thenReturn(true);
+
+        // Execute
+        BuilderMain.runMain(rc);
+
+        // Verify trap building attempt
+        verify(rc).build(eq(TrapType.EXPLOSIVE), any(MapLocation.class));
+    }
