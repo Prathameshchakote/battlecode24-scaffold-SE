45fd1493d29c3cba69e88ecdd396aca9cffe931d
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:04:44 -0800

    create AttackSetupTest.java

---
+++ test/vikingfuncsplayer/AttackSetupTest.java
@@ -0,0 +1,15 @@
+package vikingfuncsplayer;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
