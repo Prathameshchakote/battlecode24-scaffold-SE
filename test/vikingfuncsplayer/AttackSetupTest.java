40b5fce04d2b91e5352ff8b049cd2faa80bf09af
Prathamesh Chakote <chakote@pdx.edu>
2024-11-18 12:07:30 -0800

    testRunSetup_WithEmptyCrumbs

--- test/vikingfuncsplayer/AttackSetupTest.java
+++ test/vikingfuncsplayer/AttackSetupTest.java
@@ -119,3 +119,21 @@ public class AttackSetupTest {
         verify(rc).canMove(any(Direction.class));
         verify(rc, never()).move(any(Direction.class));
     }
+
+    @Test
+    public void testRunSetup_WithEmptyCrumbs() throws GameActionException {
+        // Setup
+        when(rc.getRoundNum()).thenReturn(GameConstants.SETUP_ROUNDS - 1);
+        when(rc.senseNearbyCrumbs(-1)).thenReturn(new MapLocation[]{});
+        when(rc.canMove(any(Direction.class))).thenReturn(true);
+
+        // Execute
+        AttackSetup.runSetup(rc);
+
+        // Verify
+        verify(rc).getRoundNum();
+        verify(rc).senseNearbyCrumbs(-1);
+        verify(rc).canMove(any(Direction.class));
+        verify(rc).move(any(Direction.class));
+    }
+}
